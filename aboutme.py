8ecccdf5a7ed337f9ba658c5cf97dc840e492276
Chule <46980649+ChuleHou@users.noreply.github.com>
2020-02-18 16:32:28 -0600

    Add files via upload

---
+++ aboutme.py
@@ -0,0 +1,19 @@
+#Chule Hou
+#Pawprint: chcnd
+#Student ID: 14327214
+name = 'Chule Hou'
+major = 'Computer Science'
+
+print('My name is '+ name)
+print('My major is ' + major)
+print('I am an international student')
+print('I like watching movies')
+print('I hope to make more friends')
+print('This is my first Challenge')
+
+
+
+
+
+
+
